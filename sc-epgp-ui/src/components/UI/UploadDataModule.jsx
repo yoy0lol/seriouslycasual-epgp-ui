2bd56341f35ba4f36bdb84e8a28da5181c565ffd
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2022-12-31 21:28:12 +0300

    Update UpdateDataModule to only take text inputs.

--- sc-epgp-ui/src/components/UI/UploadDataModule.jsx
+++ sc-epgp-ui/src/components/UI/UploadDataModule.jsx
@@ -1,69 +1,64 @@
 import classNames from 'classnames';
 import React, { useState, useEffect, useRef } from 'react';
+import { AiFillWarning } from 'react-icons/ai';
 
 export default function UploadDataModule() {
-  const [file, setFile] = useState(null);
-  const [data, setData] = useState(null);
-  const fileInput = useRef(null);
+  // States
+  const [jsonInput, setJsonInput] = useState('');
+  const [warning, setWarning] = useState(false);
+  const [showBtn, setShowButton] = useState(false);
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+  // Functions
+  const handleInputChange = (e) => {
+    setJsonInput(e.target.value);
   };
 
-  const clearFileInput = () => {
-    console.log('Clear File Input clicked');
-    fileInput.current ? (fileInput.current.value = '') : null;
-    setFile(null);
-    setData(null);
-  };
-
+  // Use effect to do stuff if the
   useEffect(() => {
-    if (!file) {
-      // if there's no file, the useEffect won't trigger
-      return;
+    if (jsonInput) {
+      try {
+        JSON.parse(jsonInput);
+        setWarning(false);
+        setShowButton(true);
+      } catch (error) {
+        setWarning(true);
+        setShowButton(false);
+      }
+    } else {
+      setWarning(false);
+      setShowButton(false);
     }
-
-    if (file.name !== 'epgp.json') {
-      // if the file is not named 'epgp.json' the file gets rejected and the input gets cleared
-      console.log(`Can't upload this file.`);
-      clearFileInput();
-      return;
-    }
-
-    console.log(file.name);
-
-    const reader = new FileReader();
-    reader.onloadend = (e) => {
-      const jsonData = JSON.parse(e.target.result);
-      setData(jsonData);
-    };
-    reader.readAsText(file);
-  }, [file]);
+  }, [jsonInput]);
 
   // Styles
   const defaultBtnStlye = classNames(['w-36 py-3']);
 
   return (
-    <div className='mx-auto flex flex-col space-y-10'>
-      {data ? (
-        <div className='flex flex-col'>
-          <pre className='overflow-y-scroll max-h-72 bg-gray p-5'>{JSON.stringify(data, null, 2)}</pre>
-          <div className='flex mx-auto space-x-10 my-5'>
-            {/* Clear button goes below */}
-            <button className={defaultBtnStlye + ' bg-[#991b1b]'} onClick={clearFileInput}>
-              Clear Data
-            </button>
-            {/* Post Button goes below */}
-            <button className={defaultBtnStlye + ' bg-[#15803d]'} onClick={null}>
-              Post Data
-            </button>
-          </div>
+    <div className='flex flex-col w-full space-y-10'>
+      <div className='flex flex-col'>
+        <textarea
+          placeholder='// Copy and paste the text from your JSON file here.          '
+          onChange={handleInputChange}
+          value={jsonInput}
+          rows={10}
+          cols={80}
+          className='bg-gray text-text rounded-md py-2 px-4 block w-full leading-5 font-mono'
+        />
+      </div>
+
+      {warning && (
+        <div className='rounded-md bg-secondary p-4 flex items-center align-middle'>
+          <AiFillWarning className='flex-shrink-0 text-gray h-7 w-7' />
+          <p className='ml-3 text-sm font-medium text-gray'>
+            It seems like the text you inputted is not a JSON file. Make sure you copy everything and try again.
+          </p>
         </div>
-      ) : (
-        <label className='bg-secondary font-lora py-2 px-3 w-36 text-gray text-center font-bold'>
-          Choose File
-          <input type='file' style={{ display: 'none' }} onChange={handleFileChange} ref={fileInput} />
-        </label>
+      )}
+
+      {showBtn && (
+        <button className={defaultBtnStlye + ' bg-[#15803d]'} onClick={null}>
+          Post Data
+        </button>
       )}
     </div>
   );
