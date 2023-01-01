f50a33d35f88c8ff9aabb47ca21d7970cc6678bc
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2023-01-01 12:23:24 +0300

    Get fucking wowhead tooltips in because fuck you wowhead (works with error)

--- sc-epgp-ui/src/pages/playerpage/PlayerPage.jsx
+++ sc-epgp-ui/src/pages/playerpage/PlayerPage.jsx
@@ -1,13 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 export default function PlayerPage() {
-  const { playerName } = useParams();
-  console.log(playerName);
-
   return (
     <div>
-      <p>hi</p>
-      <p>hi</p>
+      <Helmet>
+        <script>{`const whTooltips = {colorLinks: true, iconizeLinks: true, renameLinks: true};`}</script>
+        <script src='https://wow.zamimg.com/js/tooltips.js'></script>
+      </Helmet>
+      <div className='text-5xl'>
+        <p>hi</p>
+      </div>
+      <a href='https://www.wowhead.com/item=25697' className='q3' data-wowhead='gems=23121&amp;ench=2647&amp;pcs=25695:25696:25697'>
+        [Felstalker Bracers]
+      </a>
     </div>
   );
 }
