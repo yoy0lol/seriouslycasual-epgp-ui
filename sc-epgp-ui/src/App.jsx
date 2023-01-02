f0f56565a71d731f91dde8c2014bae7dcc5d7c10
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2023-01-02 22:18:17 +0300

    Add routing to NavBar

--- sc-epgp-ui/src/App.jsx
+++ sc-epgp-ui/src/App.jsx
@@ -7,18 +7,18 @@ import PlayerPage from './pages/playerpage/PlayerPage';
 
 function App() {
   return (
-    <div className='min-h-screen min-w-screen bg-gradient-to-t from-bgGr1 to-bgGr2'>
-      <NavBar />
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-text h-full min-w-full'>
-        <BrowserRouter>
+    <BrowserRouter>
+      <div className='min-h-screen min-w-screen bg-gradient-to-t from-bgGr1 to-bgGr2'>
+        <NavBar />
+        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-text h-full min-w-full'>
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/characters/:region/:realm/:playerName' element={<PlayerPage />}></Route>
             <Route path='*' element={<Navigate to='/' />}></Route>
           </Routes>
-        </BrowserRouter>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- sc-epgp-ui/src/components/Layout/NavBar.jsx
+++ sc-epgp-ui/src/components/Layout/NavBar.jsx
@@ -1,19 +1,21 @@
 import scLogo from '../../assets/sc_logo_svg_edited.svg';
 import { ReactSVG } from 'react-svg';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 
 export default function NavBar() {
   return (
     <nav className='text-text bg-navBarBg border-b-2 border-secondary overflow-x-hidden w-full'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-text h-full w-full'>
         <div className='flex overflow-hidden py-2 items-center  w-full'>
-          <div className='w-fit bg-test1 flex'>
-            <ReactSVG src={scLogo} className='h-11 w-11' style={{ color: 'red' }} />
-            <div className='px-2 flex flex-col font-poppins'>
-              <span className='font-black text-sm text-secondary leading-snug'>SeriouslyCasual</span>
-              <span className='font-light text-lg leading-snug'>EPGP Interface</span>
+          <NavLink className='text-3xl font-lobster mr-auto' to='/'>
+            <div className='w-fit bg-test1 flex'>
+              <ReactSVG src={scLogo} className='h-11 w-11' style={{ color: 'red' }} />
+              <div className='px-2 flex flex-col font-poppins'>
+                <span className='font-black text-sm text-secondary leading-snug'>SeriouslyCasual</span>
+                <span className='font-light text-lg leading-snug'>EPGP Interface</span>
+              </div>
             </div>
-          </div>
+          </NavLink>
         </div>
       </div>
     </nav>
