2016c36b24d494546c3aa0c0fb769d7d1b77d649
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2023-01-03 19:46:30 +0300

    Add a 'No loot here yet' message for players with no loot history.

--- sc-epgp-ui/src/pages/playerpage/PlayerPage.jsx
+++ sc-epgp-ui/src/pages/playerpage/PlayerPage.jsx
@@ -82,7 +82,7 @@ export default function PlayerPage() {
           </div>
           {/* Loot information Portion */}
           <div className='w-2/3 h-full'>
-            <div className='flex flex-col pl-4 space-y-4 place-items-left w-full '>
+            <div className='flex flex-col pl-4 space-y-4 place-items-left w-full'>
               {/* <div className='flex flex-col w-full'> */}
               {/* Title and sub-title: */}
               <div>
@@ -93,7 +93,15 @@ export default function PlayerPage() {
               {lootHistoryData ? <LootCards lootHistoryData={lootHistoryData} /> : <div>Loading loot history...</div>}
 
               {/* Show a 'show more' button, unless the pagination is greater or equal to total number of loot for the player*/}
-              {lootHistoryData && pagination >= lootHistoryData.totalNumberOfLoots ? (
+              {/* If there is loot data + pagination < total number of loots + loot is NOT at zero then show a "Load More" button */}
+              {lootHistoryData && pagination < lootHistoryData.totalNumberOfLoots && lootHistoryData.totalNumberOfLoots !== 0 && (
+                <button onClick={loadMoreLoot} className='font-poppins text-sm bg-secondary w-36 p-2 rounded-lg text-gray font-bold place-self-center'>
+                  Load More
+                </button>
+              )}
+
+              {/* If there is loot data + pagination > total number of loots + loot is NOT at zero then show the 'END' line */}
+              {lootHistoryData && pagination >= lootHistoryData.totalNumberOfLoots && lootHistoryData.totalNumberOfLoots !== 0 && (
                 <div className='w-36 place-self-center'>
                   <div className='flex items-center py-4'>
                     {/* <!-- The left line --> */}
@@ -104,11 +112,16 @@ export default function PlayerPage() {
                     <div className='flex-grow h-[1px] w-full bg-secondary'></div>
                   </div>
                 </div>
-              ) : (
-                <button onClick={loadMoreLoot} className='font-poppins text-sm bg-secondary w-36 p-2 rounded-lg text-gray font-bold place-self-center'>
-                  Load More
-                </button>
               )}
+
+              {/* If there is loot data + total loot is = 0 then show the 'END' line */}
+              {lootHistoryData && lootHistoryData.totalNumberOfLoots === 0 && (
+                <div className='flex flex-col max-w-fit place-self-center'>
+                  <img className='w-[150px] mb-3 place-self-center' src='/sadge_png.png' alt='' />
+                  <span className='font-poppins'>Theres no loot for you here yet.</span>
+                </div>
+              )}
+
               {/* </div> */}
             </div>
           </div>
