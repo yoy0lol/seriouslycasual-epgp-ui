68bb1736a1e0bdc6157bd4bcf327e472ffebf4a0
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2023-01-05 22:45:53 +0300

    Fix minor styling issue on LootCards

--- sc-epgp-ui/src/components/UI/LootCards.jsx
+++ sc-epgp-ui/src/components/UI/LootCards.jsx
@@ -13,7 +13,7 @@ export default function LootCards({ lootHistoryData }) {
   return (
     <div
       ref={divRef}
-      className='flex flex-col flex-grow space-y-4 pr-3 overflow-y-auto overflow-x-hidden scrollbar scrollbar-thumb-secondary scrollbar-thumb-rounded-lg scrollbar-w-2'
+      className='flex flex-col space-y-4 pr-3 pb-4 overflow-y-auto overflow-x-hidden scrollbar scrollbar-thumb-secondary scrollbar-thumb-rounded-lg scrollbar-w-2'
     >
       {lootHistoryData.loots.map((item) => {
         //Date Options
