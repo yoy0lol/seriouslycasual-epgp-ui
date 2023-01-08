7faecda4a93d171a5edf61aec923997fbb4180e9
yoy0lol <94529498+yoy0lol@users.noreply.github.com>
2023-01-08 11:31:33 +0300

    Add layout for LootHistory on the main page

--- sc-epgp-ui/src/pages/home/Home.jsx
+++ sc-epgp-ui/src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import Table from '../../components/UI/Table';
+import LootHistory from '../../components/UI/LootHistory';
 
 export default function Home() {
   return (
-    <div className='flex-grow min-h-full flex-col'>
-      <div className='flex flex-col mx-auto space-y-3 w-fit sm:w-full'>{<Table />}</div>
+    <div className='flex-grow sm:flex sm:flex-row sm:space-x-5'>
+      <div className='flex flex-col mx-auto space-y-3 w-fit sm:w-4/6'>{<Table />}</div>
+      <div className='sm:w-2/6'>
+        <LootHistory />
+      </div>
     </div>
   );
 }
